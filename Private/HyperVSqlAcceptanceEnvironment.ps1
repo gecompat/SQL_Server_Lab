93f5a93278ae48b6407701ebb461b63115693509
Gerhard Pisch <github@gecomp.at>
2026-08-09 14:12:21 +0200

    Fix Windows Server unattend password hierarchy

--- Private/HyperVSqlAcceptanceEnvironment.ps1
+++ Private/HyperVSqlAcceptanceEnvironment.ps1
@@ -98,7 +98,9 @@ function New-HyperVSqlOobeUnattendXml {
 <unattend xmlns="urn:schemas-microsoft-com:unattend">
   <settings pass="oobeSystem">
     <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <AdministratorPassword><Value>$escapedPassword</Value><PlainText>true</PlainText></AdministratorPassword>
+      <UserAccounts>
+        <AdministratorPassword><Value>$escapedPassword</Value><PlainText>true</PlainText></AdministratorPassword>
+      </UserAccounts>
       <AutoLogon><Password><Value>$escapedPassword</Value><PlainText>true</PlainText></Password><Enabled>true</Enabled><LogonCount>1</LogonCount><Username>Administrator</Username></AutoLogon>
       <RegisteredOwner>SQL_Server_Lab</RegisteredOwner>
       <TimeZone>$escapedTimeZone</TimeZone>
